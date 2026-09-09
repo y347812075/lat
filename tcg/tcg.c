y347812075/lat

commit b4d1802dd08fbff7707ab29aa2e1031051c4d9d0

latc: parallelize final AOT TB translation

tcg/tcg.c
@@ -159,6 +159,9 @@ static int tcg_out_ldst_finalize(TCGContext *s);
 
 static TCGContext **tcg_ctxs;
 static unsigned int n_tcg_ctxs;
+static unsigned int max_tcg_ctxs = 1;
+static void *aot_tb_region_start;
+static size_t aot_tb_region_size;
 TCGv_env cpu_env = 0;
 const void *tcg_code_gen_epilogue;
 uintptr_t tcg_splitwx_diff;
@@ -761,6 +764,11 @@ static void tcg_region_assign(TCGContext *s, size_t curr_region)
     }
 #endif
 
+    if (aot_tb_region_start && aot_tb_region_size) {
+        s->tb_gen_buffer = aot_tb_region_start +
+                           curr_region * aot_tb_region_size;
+        s->tb_gen_highwater = s->tb_gen_buffer + aot_tb_region_size - 1024;
+    }
     s->tb_gen_ptr = s->tb_gen_buffer;
 #if defined(CONFIG_LATX_TBMINI_ENABLE)
     /* leave space for TBMini for the 1st TB */
@@ -799,6 +807,11 @@ static bool tcg_region_alloc(TCGContext *s)
     return err;
 }
 
+bool tcg_region_alloc_aot(TCGContext *s)
+{
+    return tcg_region_alloc(s);
+}
+
 /*
  * Perform a context's first region allocation.
  * This function does _not_ increment region.agg_size_full.
@@ -832,9 +845,30 @@ void tcg_region_reset_all(void)
 }
 
 #ifdef CONFIG_USER_ONLY
+static unsigned int tcg_aot_thread_count(void)
+{
+    const char *value = getenv("LATC_AOT_THREADS");
+    unsigned long requested;
+
+    if (value && *value) {
+        requested = strtoul(value, NULL, 10);
+    } else if (getenv("LATC_EMIT_AOT")) {
+        long online = sysconf(_SC_NPROCESSORS_ONLN);
+        requested = online > 0 ? online : 1;
+        requested = MIN(requested, 8ul);
+    } else {
+        requested = 1;
+    }
+    return MIN(MAX(requested, 1ul), 32ul);
+}
+
 static size_t tcg_n_regions(void)
 {
-    return 1;
+    max_tcg_ctxs = tcg_aot_thread_count();
+    /* Address ranges do not produce equal amounts of host code.  Keep a
+     * second region per compiler thread in the shared allocator so a busy
+     * worker can continue instead of truncating its TB slice. */
+    return max_tcg_ctxs * 2;
 }
 #else
 /*
@@ -912,6 +946,12 @@ void tcg_region_init(void)
     size_t i;
 
     n_regions = tcg_n_regions();
+#ifdef CONFIG_USER_ONLY
+    if (n_regions > 1 && tcg_init_ctx.tb_gen_buffer) {
+        aot_tb_region_start = tcg_init_ctx.tb_gen_buffer;
+        aot_tb_region_size = tcg_init_ctx.code_gen_buffer_size / n_regions;
+    }
+#endif
 
     /* The first region will be 'aligned - buf' bytes larger than the others */
     aligned = QEMU_ALIGN_PTR_UP(buf, page_size);
@@ -1021,7 +1061,32 @@ static void alloc_tcg_plugin_context(TCGContext *s)
 #ifdef CONFIG_USER_ONLY
 void tcg_register_thread(void)
 {
-    tcg_ctx = &tcg_init_ctx;
+    unsigned int i, n;
+    bool err;
+
+    if (max_tcg_ctxs == 1) {
+        tcg_ctx = &tcg_init_ctx;
+        return;
+    }
+
+    TCGContext *s = g_malloc(sizeof(*s));
+    *s = tcg_init_ctx;
+    for (i = 0, n = tcg_init_ctx.nb_globals; i < n; ++i) {
+        if (tcg_init_ctx.temps[i].mem_base) {
+            ptrdiff_t b = tcg_init_ctx.temps[i].mem_base - tcg_init_ctx.temps;
+            tcg_debug_assert(b >= 0 && b < n);
+            s->temps[i].mem_base = &s->temps[b];
+        }
+    }
+    n = qatomic_fetch_inc(&n_tcg_ctxs);
+    g_assert(n < max_tcg_ctxs);
+    qatomic_set(&tcg_ctxs[n], s);
+    alloc_tcg_plugin_context(s);
+    tcg_ctx = s;
+    qemu_mutex_lock(&region.lock);
+    err = tcg_region_initial_alloc__locked(s);
+    g_assert(!err);
+    qemu_mutex_unlock(&region.lock);
 }
 #else
 void tcg_register_thread(void)
@@ -1253,7 +1318,9 @@ void tcg_context_init(TCGContext *s)
      * In softmmu we will have at most max_cpus TCG threads.
      */
 #ifdef CONFIG_USER_ONLY
-    tcg_ctxs = &tcg_ctx;
+    max_tcg_ctxs = tcg_aot_thread_count();
+    tcg_ctxs = g_new0(TCGContext *, max_tcg_ctxs);
+    tcg_ctxs[0] = tcg_ctx;
     n_tcg_ctxs = 1;
 #else
     MachineState *ms = MACHINE(qdev_get_machine());

tools/latc/lat/tcg/tcg.c
@@ -159,6 +159,9 @@ static int tcg_out_ldst_finalize(TCGContext *s);
 
 static TCGContext **tcg_ctxs;
 static unsigned int n_tcg_ctxs;
+static unsigned int max_tcg_ctxs = 1;
+static void *aot_tb_region_start;
+static size_t aot_tb_region_size;
 TCGv_env cpu_env = 0;
 const void *tcg_code_gen_epilogue;
 uintptr_t tcg_splitwx_diff;
@@ -761,6 +764,11 @@ static void tcg_region_assign(TCGContext *s, size_t curr_region)
     }
 #endif
 
+    if (aot_tb_region_start && aot_tb_region_size) {
+        s->tb_gen_buffer = aot_tb_region_start +
+                           curr_region * aot_tb_region_size;
+        s->tb_gen_highwater = s->tb_gen_buffer + aot_tb_region_size - 1024;
+    }
     s->tb_gen_ptr = s->tb_gen_buffer;
 #if defined(CONFIG_LATX_TBMINI_ENABLE)
     /* leave space for TBMini for the 1st TB */
@@ -799,6 +807,11 @@ static bool tcg_region_alloc(TCGContext *s)
     return err;
 }
 
+bool tcg_region_alloc_aot(TCGContext *s)
+{
+    return tcg_region_alloc(s);
+}
+
 /*
  * Perform a context's first region allocation.
  * This function does _not_ increment region.agg_size_full.
@@ -832,9 +845,30 @@ void tcg_region_reset_all(void)
 }
 
 #ifdef CONFIG_USER_ONLY
+static unsigned int tcg_aot_thread_count(void)
+{
+    const char *value = getenv("LATC_AOT_THREADS");
+    unsigned long requested;
+
+    if (value && *value) {
+        requested = strtoul(value, NULL, 10);
+    } else if (getenv("LATC_EMIT_AOT")) {
+        long online = sysconf(_SC_NPROCESSORS_ONLN);
+        requested = online > 0 ? online : 1;
+        requested = MIN(requested, 8ul);
+    } else {
+        requested = 1;
+    }
+    return MIN(MAX(requested, 1ul), 32ul);
+}
+
 static size_t tcg_n_regions(void)
 {
-    return 1;
+    max_tcg_ctxs = tcg_aot_thread_count();
+    /* Address ranges do not produce equal amounts of host code.  Keep a
+     * second region per compiler thread in the shared allocator so a busy
+     * worker can continue instead of truncating its TB slice. */
+    return max_tcg_ctxs * 2;
 }
 #else
 /*
@@ -912,6 +946,12 @@ void tcg_region_init(void)
     size_t i;
 
     n_regions = tcg_n_regions();
+#ifdef CONFIG_USER_ONLY
+    if (n_regions > 1 && tcg_init_ctx.tb_gen_buffer) {
+        aot_tb_region_start = tcg_init_ctx.tb_gen_buffer;
+        aot_tb_region_size = tcg_init_ctx.code_gen_buffer_size / n_regions;
+    }
+#endif
 
     /* The first region will be 'aligned - buf' bytes larger than the others */
     aligned = QEMU_ALIGN_PTR_UP(buf, page_size);
@@ -1021,7 +1061,32 @@ static void alloc_tcg_plugin_context(TCGContext *s)
 #ifdef CONFIG_USER_ONLY
 void tcg_register_thread(void)
 {
-    tcg_ctx = &tcg_init_ctx;
+    unsigned int i, n;
+    bool err;
+
+    if (max_tcg_ctxs == 1) {
+        tcg_ctx = &tcg_init_ctx;
+        return;
+    }
+
+    TCGContext *s = g_malloc(sizeof(*s));
+    *s = tcg_init_ctx;
+    for (i = 0, n = tcg_init_ctx.nb_globals; i < n; ++i) {
+        if (tcg_init_ctx.temps[i].mem_base) {
+            ptrdiff_t b = tcg_init_ctx.temps[i].mem_base - tcg_init_ctx.temps;
+            tcg_debug_assert(b >= 0 && b < n);
+            s->temps[i].mem_base = &s->temps[b];
+        }
+    }
+    n = qatomic_fetch_inc(&n_tcg_ctxs);
+    g_assert(n < max_tcg_ctxs);
+    qatomic_set(&tcg_ctxs[n], s);
+    alloc_tcg_plugin_context(s);
+    tcg_ctx = s;
+    qemu_mutex_lock(&region.lock);
+    err = tcg_region_initial_alloc__locked(s);
+    g_assert(!err);
+    qemu_mutex_unlock(&region.lock);
 }
 #else
 void tcg_register_thread(void)
@@ -1253,7 +1318,9 @@ void tcg_context_init(TCGContext *s)
      * In softmmu we will have at most max_cpus TCG threads.
      */
 #ifdef CONFIG_USER_ONLY
-    tcg_ctxs = &tcg_ctx;
+    max_tcg_ctxs = tcg_aot_thread_count();
+    tcg_ctxs = g_new0(TCGContext *, max_tcg_ctxs);
+    tcg_ctxs[0] = tcg_ctx;
     n_tcg_ctxs = 1;
 #else
     MachineState *ms = MACHINE(qdev_get_machine());

accel/tcg/translate-all.c
@@ -990,7 +990,9 @@ static void page_lock_pair(PageDesc **ret_p1, tb_page_addr_t phys1,
     tb_page_addr_t page1;
     tb_page_addr_t page2;
 
-    assert_memory_lock();
+    if (!aot_parallel_translate) {
+        assert_memory_lock();
+    }
     g_assert(phys1 != -1);
 
     page1 = phys1 >> TARGET_PAGE_BITS;
@@ -2205,7 +2207,8 @@ TranslationBlock *tb_gen_code(CPUState *cpu,
      * but the paired page cross 16K boundary is still not protected.
      */
     int p_flags = page_get_flags(pc);
-    if ((p_flags & PAGE_WRITE) && !page_is_shadow_not_shmm(pc)) {
+    if (!aot_parallel_translate && (p_flags & PAGE_WRITE) &&
+        !page_is_shadow_not_shmm(pc)) {
         mprotect((void*)(pc & qemu_host_page_mask), qemu_host_page_size, PROT_READ);
     }
 

include/tcg/tcg.h
@@ -905,6 +905,7 @@ TranslationBlock *tcg_tb_alloc(TCGContext *s);
 TranslationBlock *tcg_tb_alloc_full(TCGContext *s);
 
 void tcg_region_init(void);
+bool tcg_region_alloc_aot(TCGContext *s);
 void tb_destroy(TranslationBlock *tb);
 void tcg_region_reset_all(void);
 

linux-user/latc-bundle-loader.c
@@ -6,6 +6,7 @@
 
 #include "accel/tcg/internal.h"
 #include "exec/exec-all.h"
+#include "aot.h"
 #include "jrra.h"
 #include "tcg/tcg.h"
 
@@ -671,6 +672,36 @@ void latc_bundle_pretranslate(struct CPUState *cpu, uint64_t guest_entry)
         pending_tb_hash, pending_tb_equal, g_free, NULL);
     GArray *pending = g_array_new(FALSE, FALSE, sizeof(LatcPendingTb));
     uint64_t pretranslate_started = monotonic_ns();
+
+    if (getenv("LATC_EMIT_AOT")) {
+        for (uint64_t i = 0; i < header.tb_count; i++) {
+            LatcDiskTb disk_tb;
+            if (read_cfg(&disk_tb, sizeof(disk_tb),
+                         tb_offset + i * sizeof(disk_tb))) {
+                failed++;
+                continue;
+            }
+            if (!disk_tb.selected) {
+                continue;
+            }
+            selected++;
+            uint64_t pc = disk_tb.start + load_bias;
+            if (pc < disk_tb.start || !program_address(pc)) {
+                failed++;
+                continue;
+            }
+            uint32_t tb_cflags = tbset_cflags(cflags,
+                                              disk_tb.semantic_flags);
+            aot_compile_key_add(pc, tb_cflags, IS_CODE64);
+        }
+        stat_cfg_tbs = header.tb_count;
+        stat_selected = selected;
+        stat_pretranslated = selected;
+        stat_failed = failed;
+        pretranslation_complete = true;
+        write_stats();
+        return;
+    }
 
     /* The TB set is authoritative.  Do not compile the rest of the static CFG. */
     for (uint64_t i = 0; i < header.tb_count; i++) {

target/i386/latx/include/aot.h
@@ -288,7 +288,7 @@ typedef struct aot_rel {
 
 } aot_rel;
 
-extern aot_rel *rel_table;
+extern __thread aot_rel *rel_table;
 extern seg_info **seg_info_vector;
 void mk_aot_dir(char * pathname);
 void aot_set_process_profile(int argc, char **argv);
@@ -305,6 +305,9 @@ int add_rel_entry(aot_rel_kind kind, uint32_t **tc_offset,
                   target_ulong extra_addent);
 int aot_rel_table_checkpoint(void);
 void aot_rel_table_rollback(int checkpoint);
+aot_rel *aot_rel_table_release(int *count);
+void aot_rel_table_merge(aot_rel *entries, int count,
+                         TranslationBlock **tbs, int tb_count);
 typedef enum AOTExitReason {
     AOT_EXIT_THREAD,
     AOT_EXIT_FINAL,
@@ -314,6 +317,8 @@ void aot_exit_entry(CPUState *cpu, AOTExitReason reason);
 void aot_init(void);
 target_ulong aot_get_call_offset(ADDRX addr);
 void aot_generate(CPUState *cpu);
+void aot_compile_key_add(target_ulong pc, uint32_t cflags,
+                         uint16_t bool_flags);
 int aot_get_file_init(char *aot_file);
 void clear_rel_table(void);
 void recover_aot_tb(char *lib_name, uint64_t aot_offset,

target/i386/latx/include/capstone_git.h
@@ -9,7 +9,7 @@
 #include "latx-disassemble-trace.h"
 #include "../capstone_git/include/capstone/capstone.h"
 
-extern csh handle[2];
+extern __thread csh handle[2];
 struct la_dt_insn *nextcapstone_get_from_insn(cs_insn *inputinfo,
         int ir1_num, void *pir1_base);
 cs_insn *nextcapstone_post(struct la_dt_insn *inputinfo,

target/i386/latx/include/ir1.h
@@ -31,7 +31,7 @@
 #else
 #define CS_MODE CS_MODE_64
 #endif
-extern csh handle[2];
+extern __thread csh handle[2];
 
 
 typedef enum {

commit 2863d4f81991a665dfc02d5bf57d7435eb39558e

LATX, fix: Restrict AOT translation contexts to export mode
LATC_AOT_THREADS affected normal guest execution as well as offline
export. With four contexts, a guest thread's TB exit address could not
be found, leaving its PC unchanged and repeatedly executing the thread
entry prologue until its stack overflowed.

Keep normal guest execution at one TCG context and apply the thread
setting only when LATC_EMIT_AOT is present. Register tcg.c as a generated
source so the standalone compiler import follows the authoritative copy.
Add a Python threading integration test for values 1, 4 and 16; it exits
139 on the previous runner and passes with this change.

Validated with full latc tests, source sync, fresh install, fork,
dynamic semantics, invalidation and the four-application strict suite.
The binfmt interpreter and explicit runner used the same binary.

Fixes: b4d1802dd08f ("latc: parallelize final AOT TB translation")
Signed-off-by: yuerengan <y347812075@163.com>

tcg/tcg.c
@@ -850,6 +850,11 @@ static unsigned int tcg_aot_thread_count(void)
     const char *value = getenv("LATC_AOT_THREADS");
     unsigned long requested;
 
+    /* Export workers have private contexts; guest threads share the JIT. */
+    if (!getenv("LATC_EMIT_AOT")) {
+        return 1;
+    }
+
     if (value && *value) {
         requested = strtoul(value, NULL, 10);
     } else if (getenv("LATC_EMIT_AOT")) {

tools/latc/Makefile
@@ -1042,3 +1042,11 @@ test-invalidation-stats:
 	python3 tests/test-normalize-invalidation-stats.py
 
 test: test-invalidation-stats
+
+.PHONY: test-aot-v2-thread-env
+test-aot-v2-thread-env:
+	@test -n "$(AOT_V2_RUNNER)" -a -n "$(AOT_V2_RUNTIME_DIR)" \
+	  -a -n "$(X86_ROOTFS)"
+	sh tests/test-aot-v2-thread-env.sh $(AOT_V2_RUNNER) \
+	  $(AOT_V2_RUNTIME_DIR) $(X86_ROOTFS) \
+	  $(abspath $(BUILD)/aot-v2-thread-env-test)

tools/latc/lat/tcg/tcg.c
@@ -850,6 +850,11 @@ static unsigned int tcg_aot_thread_count(void)
     const char *value = getenv("LATC_AOT_THREADS");
     unsigned long requested;
 
+    /* Export workers have private contexts; guest threads share the JIT. */
+    if (!getenv("LATC_EMIT_AOT")) {
+        return 1;
+    }
+
     if (value && *value) {
         requested = strtoul(value, NULL, 10);
     } else if (getenv("LATC_EMIT_AOT")) {

tools/latc/tests/test-aot-v2-thread-env.sh
@@ -1,0 +1,26 @@
+#!/bin/sh
+set -eu
+runner=$1
+runtime=$2
+rootfs=$3
+work=$4
+mkdir -p "$work"
+for threads in 1 4 16; do
+    env -u LATC_EMIT_AOT -u LATX_AOT_V2_CACHE_DIR \
+      -u LATX_AOT_V2_MODULE -u LATX_AOT_V2_LATCD_SOCKET \
+      LD_LIBRARY_PATH="$runtime" LATX_AOT=0 LATC_DISABLE_PRETRANSLATE=1 \
+      LATC_AOT_THREADS="$threads" \
+      timeout -k 2s 30s "$runner" -L "$rootfs" \
+      "$rootfs/usr/bin/python3" -S -c '
+import threading
+results = []
+for i in range(12):
+    thread = threading.Thread(target=lambda: results.append(sum(range(100))))
+    thread.start()
+    thread.join()
+assert results == [4950] * 12, results
+print("THREAD_ENV_OK")
+' >"$work/$threads.out" 2>"$work/$threads.err"
+    grep -qx THREAD_ENV_OK "$work/$threads.out"
+done
+echo 'test-aot-v2-thread-env: PASS'